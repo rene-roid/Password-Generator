67a0576e63b73be8927fd73d6ef2c1fb8a7aea4d
Yuuki <52652158+rene-roid@users.noreply.github.com>
2023-02-26 19:16:34 +0100

    save settings

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,46 @@ let length = document.getElementById("length");
 let includeUppercase = document.getElementById("uppercase");
 let includeNumbers = document.getElementById("numbers");
 let includeSymbols = document.getElementById("symbols");
+let save = document.getElementById("save_settings_checkbox");
 
 let input_result = document.getElementById("pass_result");
+
+let save_to_localstorage = () => {
+
+    if (save.checked) {
+        localStorage.setItem("length", length.value);
+        localStorage.setItem("includeUppercase", includeUppercase.checked);
+        localStorage.setItem("includeNumbers", includeNumbers.checked);
+        localStorage.setItem("includeSymbols", includeSymbols.checked);
+        localStorage.setItem("save", save.checked);
+    } else {
+        localStorage.removeItem("length");
+        localStorage.removeItem("includeUppercase");
+        localStorage.removeItem("includeNumbers");
+        localStorage.removeItem("includeSymbols");
+        localStorage.removeItem("save");
+    }
+}
+
+let load_from_localstorage = () => {
+    if (localStorage.getItem("length") != null) {
+        length.value = localStorage.getItem("length");
+        includeUppercase.checked = localStorage.getItem("includeUppercase") == "true";
+        includeNumbers.checked = localStorage.getItem("includeNumbers") == "true";
+        includeSymbols.checked = localStorage.getItem("includeSymbols") == "true";
+        save.checked = localStorage.getItem("save") == "true";
+    }   
+}
+
+let reset_settings = () => {
+    length.value = 16;
+    includeUppercase.checked = true;
+    includeNumbers.checked = true;
+    includeSymbols.checked = false;
+    save_to_localstorage();
+}
+
+load_from_localstorage()
 
 let curr_random = () => {
     let final_string = "";
@@ -34,6 +72,7 @@ let generatePassword = () => {
         password += final_string[Math.floor(Math.random() * final_string.length)];
     }
     input_result.value = password;
+    save_to_localstorage();
 }
 
 let copyPassword = () => {
@@ -42,4 +81,12 @@ let copyPassword = () => {
     // alert("Password copied to clipboard");
 }
 
-generatePassword();
+generatePassword()
+
+let save_settings_button = () => {
+    if (save.checked) {
+        save_to_localstorage();
+    } else {
+        reset_settings();
+    }
+}
